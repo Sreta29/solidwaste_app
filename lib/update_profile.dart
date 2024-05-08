cc3e44697d1a83f119994d04d1baadb510c443ad
Sreta29 <sreta290101@gmail.com>
2024-05-09 02:11:18 +0800

    Code inserted to resolve overfloe issue

--- lib/update_profile.dart
+++ lib/update_profile.dart
@@ -3,28 +3,25 @@
 import 'package:flutter/material.dart';
 import 'package:solidwaste_app/homepage.dart';
 
-
-class UpdateProfilePage extends StatefulWidget{
-    const UpdateProfilePage({super.key, required String userName, required String userProfilePicture});
+class UpdateProfilePage extends StatefulWidget {
+  const UpdateProfilePage(
+      {super.key,
+      required String userName,
+      required String userProfilePicture});
 
   @override
   State<UpdateProfilePage> createState() => _UpdateProfilePageState();
 }
 
-class _UpdateProfilePageState extends State<UpdateProfilePage>{
-
+class _UpdateProfilePageState extends State<UpdateProfilePage> {
   bool changePage = false;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
 
-    setState(() {
-          
-        });
+    setState(() {});
   }
-
-
 
   final _formKey = GlobalKey<FormState>();
 
@@ -33,111 +30,113 @@ class _UpdateProfilePageState extends State<UpdateProfilePage>{
 
   @override
   Widget build(BuildContext context) {
-    if(changePage){
-      return const HomePage(key: null,);
+    if (changePage) {
+      return const HomePage(
+        key: null,
+      );
     }
-    return
-    Scaffold(
-        appBar: 
-      AppBar(
-        // Display the back button in the app bar
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            setState(() {
-              changePage = !changePage;
-            });           
-          },
+    return Scaffold(
+        appBar: AppBar(
+          // Display the back button in the app bar
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              setState(() {
+                changePage = !changePage;
+              });
+            },
+          ),
+          title: const Text('Update Profile Page'),
         ),
-        title: const Text('Update Profile Page'),
-      ),
-      body:  Form(
-      key: _formKey,
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Name'),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please enter your name';
-                }
-                return null;
-              },
-              onSaved: (value) => _name = value!,
+        body: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Name'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your name';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _name = value!,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'User Name'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your user name';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _userName = value!,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Email'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your email';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _email = value!,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Password'),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _password = value!,
+                ),
+                TextFormField(
+                  decoration:
+                      const InputDecoration(labelText: 'Confirm Password'),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please confirm your password';
+                    } else if (value != _password) {
+                      return 'Passwords do not match';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _confirmPassword = value!,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Address'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your address';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _address = value!,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                        width: 150, // Set the width as per your requirement
+                        height: 50, // Set the height as per your requirement
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('Update Profile'),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+
+              ],
             ),
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'User Name'),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please enter your user name';
-                }
-                return null;
-              },
-              onSaved: (value) => _userName = value!,
-            ),
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Email'),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please enter your email';
-                }
-                return null;
-              },
-              onSaved: (value) => _email = value!,
-            ),
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Password'),
-              obscureText: true,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please enter your password';
-                }
-                return null;
-              },
-              onSaved: (value) => _password = value!,
-            ),
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Confirm Password'),
-              obscureText: true,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please confirm your password';
-                } else if (value != _password) {
-                  return 'Passwords do not match';
-                }
-                return null;
-              },
-              onSaved: (value) => _confirmPassword = value!,
-            ),
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Address'),
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please enter your address';
-                }
-                return null;
-              },
-              onSaved: (value) => _address = value!,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _formKey.currentState!.save();
-                    // Perform form submission logic here.
-                  }
-                },
-                child: const Text('Update Profile'),
-              ),
-            ),
-          ],
-        ),
-      ),
-    )
-    );  
+          ),
+        ));
   }
 }
